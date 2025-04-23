c138cda96afa9684eeb07259f31c2fe0e393862e
vvduth <ducthai060501@gmail.com>
2025-04-23 09:54:59 +0300

    screen set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen
+            name="notification"
+            options={{ title: "Notification" }}
+          />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={
+        styles.container
+      }
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/notification"}>Visit nofitycation</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 50
+  }
+})
